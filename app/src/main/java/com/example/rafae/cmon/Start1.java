bf43e3be83995c5227da7a3cdce26f1101eb91f6
Alba <little.sheena.x7@gmail.com>
2016-02-08 20:54:32 +0100

    Pregunta1 buena

--- app/src/main/java/com/example/rafae/cmon/Start1.java
+++ app/src/main/java/com/example/rafae/cmon/Start1.java
@@ -1,7 +1,9 @@
 package com.example.rafae.cmon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Start1 extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Start1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start1);
+    }
+
+    public void Pregunta2(View view)
+    {
+        Intent intent = new Intent(this, Start2.class);
+        startActivity(intent);
     }
 }
